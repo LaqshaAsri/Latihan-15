cd5f7d95380020de4b0293aab37cca9fe7cea685
LaqshaAsri <laqshaasri551@gmail.com>
2026-06-25 13:43:21 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const routes = require('./routes/index');
+app.use('/', routes);
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
